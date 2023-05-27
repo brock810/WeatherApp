4483dd727745d6a71992f4144e5e74671bef8ad3
brock810 <130688727+brock810@users.noreply.github.com>
2023-05-27 14:25:06 -0500

    another file

---
+++ dot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+           // Name = "Brock";
+
+           // color = "Blue";
+
+           // drink = "Whiskey";
+           
+           // animal = "cats";
+           
+
+            Console.WriteLine("What is your name?"); // output
+            var userName = Console.ReadLine(); // input
+
+            Console.WriteLine($"Hi {userName} What is your favorite color?");
+            var color = Console.ReadLine(); // input
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite drink?"); // output
+            var drink = Console.ReadLine(); // input
+
+            Console.WriteLine($"Whiskey! good choice!");
+            
+
+
+        }
+
+
+    }
+
+}
+
